a0e4d7309eacff5ab06dee0617fffe5a75f6f95b
blanklist <joehineriv@gmail.com>
2017-06-28 09:54:35 -0700

    adds user and session files. untested

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  has_secure_password
+
+end
