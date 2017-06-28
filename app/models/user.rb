d6c54a0432b938d69299e2bcb5e0e52b55a36d7d
blanklist <joehineriv@gmail.com>
2017-06-28 10:16:18 -0700

    user auth function tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
